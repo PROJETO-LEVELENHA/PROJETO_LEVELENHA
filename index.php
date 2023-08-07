f77707f1384b81d4cbedb37fb0936c72c9b3e240
Gustavo Nascimento <123gustaf321@gmail.com>
2023-08-07 01:19:14 -0300

    header log-out feito

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LeveLenha</title>
+
+    <!-- FONTE -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- ESTILOS -->
+    <link rel="stylesheet" href="assets/CSS/base.css">
+    <link rel="stylesheet" href="assets/CSS/header.css">
+    <link rel="stylesheet" href="assets/CSS/footer.css">
+</head>
+<body class="grid-hp">
+    <?php
+        require_once('ASSETS/HTML/header-log-out.html');
+    ?>
+    <main>
+
+    </main>
+    <?php
+        require_once('ASSETS/HTML/footer.html');
+    ?>
+</body>
+</html>
