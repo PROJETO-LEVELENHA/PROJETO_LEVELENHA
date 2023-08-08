40ddd35956fbff469ebd720aa5e762143ab9a907
Gustavo Nascimento <123gustaf321@gmail.com>
2023-08-08 09:27:04 -0300

    add pessoal

---
+++ PESSOAL/meu-perfil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Perfil</title>
+
+    <?php
+        require_once('../ASSETS/HTML/LINKs/links-before.html');
+    ?>
+
+    <!-- EXCLUSIVO -->
+
+    <link rel="stylesheet" href="../ASSETS/CSS/aside-pessoal.css">
+</head>
+<body class="grid-pessoal">
+    <?php
+        require_once('../ASSETS/HTML/HEADERs/header-base-before.html');
+    ?>
+    <?php
+        require_once('../ASSETS/HTML/aside.html');
+    ?>
+    <main>
+
+    </main>
+</body>
+</html>
